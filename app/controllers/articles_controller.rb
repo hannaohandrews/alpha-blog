524ff7404f95887563bca2e70e2851844f982b4d
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2022-07-06 22:25:32 -0700

    added user table

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show,:edit,:update, :destroy]
+
+  def show
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to article_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
