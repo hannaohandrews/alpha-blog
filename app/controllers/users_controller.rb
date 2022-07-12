a3b0a513d97f12054d078d81a539afbc2676b24b
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2022-07-11 23:12:49 -0700

    add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ def new
   @user = User.new
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:notice] = "Your info was successfully edited"
+    redirect_to articles_path
+  else
+    render 'edit'
+  end
+end
+
 def create
  @user = User.new(user_params)
  if @user.save
@@ -14,7 +28,7 @@ def create
  end
 end
 
-private 
+private
 def user_params
   params.require(:user).permit(:username, :email, :password)
 end
